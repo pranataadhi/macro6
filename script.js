efccc08379737d4b413ce854a651edf19ac8acbe
pranataadhi <nengahadhi22@gmail.com>
2023-10-12 13:26:21 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+function kost() {
+    document.getElementById('div_kost').style.display = "block";
+    document.getElementById('div_apart').style.display = "none";
+    document.getElementById('div_rumah').style.display = "none";
+    document.getElementById('div_kantor').style.display = "none";
+    document.getElementById('div_caffe').style.display = "none";
+    document.getElementById('div_outsourcing').style.display = "none";
+
+
+}
+function apart() {
+    document.getElementById('div_kost').style.display = "none";
+    document.getElementById('div_apart').style.display = "block";
+    document.getElementById('div_rumah').style.display = "none";
+    document.getElementById('div_kantor').style.display = "none";
+    document.getElementById('div_caffe').style.display = "none";
+    document.getElementById('div_outsourcing').style.display = "none";
+
+
+}
+function rumah() {
+    document.getElementById('div_rumah').style.display = "block";
+    document.getElementById('div_apart').style.display = "none";
+    document.getElementById('div_kost').style.display = "none";
+    document.getElementById('div_kantor').style.display = "none";
+    document.getElementById('div_caffe').style.display = "none";
+    document.getElementById('div_outsourcing').style.display = "none";
+
+
+}
+function kantor() {
+    document.getElementById('div_kantor').style.display = "block";
+    document.getElementById('div_apart').style.display = "none";
+    document.getElementById('div_kost').style.display = "none";
+    document.getElementById('div_rumah').style.display = "none";
+    document.getElementById('div_caffe').style.display = "none";
+    document.getElementById('div_outsourcing').style.display = "none";
+
+
+}
+function caffe() {
+    document.getElementById('div_kantor').style.display = "none";
+    document.getElementById('div_apart').style.display = "none";
+    document.getElementById('div_kost').style.display = "none";
+    document.getElementById('div_rumah').style.display = "none";
+    document.getElementById('div_caffe').style.display = "block";
+    document.getElementById('div_outsourcing').style.display = "none";
+
+
+}
+function outsourcing() {
+    document.getElementById('div_kantor').style.display = "none";
+    document.getElementById('div_apart').style.display = "none";
+    document.getElementById('div_kost').style.display = "none";
+    document.getElementById('div_rumah').style.display = "none";
+    document.getElementById('div_caffe').style.display = "none";
+    document.getElementById('div_outsourcing').style.display = "block";
+
+}
